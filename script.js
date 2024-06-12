5c205d02cf8fb21ccaedc8dbf763b5d319868fd0
ShmPt <cacahouettetbs@gmail.com>
2024-06-12 13:08:24 +0200

    fix: path to api

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('randomJokeButton').addEventListener('click', async () => {
-    const res = await fetch('');
+    const res = await fetch('/api/jokes/random');
     const joke = await res.json();
     document.getElementById('randomJokeResult').innerText = `${joke.question}\n\n${joke.answer}`;
 });
