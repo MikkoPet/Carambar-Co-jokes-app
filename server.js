5470b6db2b37e315085ad3c4d1e459d9ce585064
ShmPt <cacahouettetbs@gmail.com>
2024-06-12 13:08:54 +0200

    fix: annotate non yet setup links to middleware

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 
 const path = require('path');
 const jokeRoutes = require('./routes/jokeRoutes');
-const setUpSwagger = require('./swagger/swagger');
-
+/* const setUpSwagger = require('./swagger/swagger');
+ */
 const PORT = process.env.PORT || 3000;
 
 //config
-setUpSwagger(app);
+/* setUpSwagger(app); */
 
 //middleware
-app.use(require('express').json());
-app.use(require('cors'));
-
+app.use(express.json());
+/* app.use(require('cors'));
+ */
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(_dirname, 'index.html'))
+    res.sendFile(path.join(__dirname, 'index.html'))
 });
 
 app.use('api/jokes', jokeRoutes);
